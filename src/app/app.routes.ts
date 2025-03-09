23d00c0a0fab96fd819268eb3c5128df2619182b
405910-Angeletti-Gonzalo <165039217+405910-Angeletti-Gonzalo@users.noreply.github.com>
2025-03-09 20:37:20 -0300

    finde

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InformacionComponent } from './informacion/informacion.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InformacionComponent }, // Página de inicio
+  { path: 'login', component: LoginComponent }   // Ruta para el login
+];
+
